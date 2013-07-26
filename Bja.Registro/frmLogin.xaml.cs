9c78daed172b924370e11ac0dad7333475495af5
rrsc <rrscgyov@gmail.com>
2013-07-26 11:06:02 +0200

    Ajustando de nuevo

---
+++ Bja.Registro/frmLogin.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Bja.Entidades;
+using Bja.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bja.Registro
+{
+  /// <summary>
+  /// Lógica de interacción para frmLogin.xaml
+  /// </summary>
+  public partial class frmLogin : Window
+  {
+    public frmLogin()
+    {
+      this.Cursor = Cursors.Wait;
+      InitializeComponent();
+      InicializacionBD.inicializarBD();
+      this.Cursor = Cursors.Arrow;
+    }
+
+    private void cmdAceptar_Click(object sender, RoutedEventArgs e)
+    {
+      //authenticar usuario
+      var rbac = new Rbac();
+      User user = rbac.authenticate(txtUsuario.Text, pasContrasena.Password);
+      if (user != null)
+      {
+        //MessageBox.Show("Acceso concedido.","Mensaje");
+        //inicia session
+        SessionManager.initSession(user);
+        //rrscgyov
+        this.Hide();
+
+        frmPrincipal objPrincipal = new frmPrincipal();
+        bool? Resultado = objPrincipal.ShowDialog();
+        if (Resultado == false)
+        {
+          objPrincipal.Close();
+          Application.Current.Shutdown();
+        }
+      }
+      else
+      {
+        MessageBox.Show("Usuario o clave no válido.", "Error");
+      }
+    }
+
+    private void cmdSalir_Click(object sender, RoutedEventArgs e)
+    {
+      this.Close();
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      txtUsuario.Focus();
+    }
+
+  }
+}
---
+++ Bja.Registro/frmPrincipal.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Bja.Entidades;
+using Bja.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bja.Registro
+{
+  /// <summary>
+  /// Lógica de interacción para frmPrincipal.xaml
+  /// </summary>
+  public partial class frmPrincipal : Window
+  {
+    public frmPrincipal()
+    {
+      this.Cursor = Cursors.Wait;
+      InitializeComponent();
+      this.Cursor = Cursors.Arrow;
+    }
+
+    private void cmdSalir_Click(object sender, RoutedEventArgs e)
+    {
+      this.Close();
+    }
+
+    private void cmdMadres_Click(object sender, RoutedEventArgs e)
+    {
+      //this.Cursor = Cursors.Wait;
+      //frmMadres objMadresWindow = new frmMadres();
+      //objMadresWindow.Owner = this;
+      //objMadresWindow.ShowDialog();
+      //objMadresWindow = null;
+      //this.Cursor = Cursors.Arrow;
+    }
+  }
+}
